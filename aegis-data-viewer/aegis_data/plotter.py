7bb88a3d4f2f3ff6e69ca14ab80e1bc554e94546
Labros Zotos <zotos.lab@gmail.com>
2024-07-25 23:18:10 +0300

    fix: Fix stylin of plots

--- aegis-data-viewer/aegis_data/plotter.py
+++ aegis-data-viewer/aegis_data/plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
 def plot_temperature(df, save_path=None):
@@ -6,7 +7,7 @@ def plot_temperature(df, save_path=None):
     plt.plot(df["datetime"], df["temperature"], label="Temperature")
     plt.xlabel("Datetime")
     plt.ylabel("Temperature")
-    plt.title("Temperature Over Time")
+    plt.title("Temperature")
     plt.legend()
     if save_path:
         plt.savefig(save_path)
@@ -18,7 +19,7 @@ def plot_humidity(df, save_path=None):
     plt.plot(df["datetime"], df["humidity"], label="Humidity", color="orange")
     plt.xlabel("Datetime")
     plt.ylabel("Humidity")
-    plt.title("Humidity Over Time")
+    plt.title("Humidity")
     plt.legend()
     if save_path:
         plt.savefig(save_path)
@@ -31,7 +32,7 @@ def plot_pump_status(df, save_path=None):
              color="green")
     plt.xlabel("Datetime")
     plt.ylabel("Pump Status")
-    plt.title("Pump Status Over Time")
+    plt.title("Pump Status")
     plt.legend()
     if save_path:
         plt.savefig(save_path)
@@ -48,22 +49,24 @@ def plot_sensor_data(df, start_date=None, end_date=None, save_path=None):
 
     axs[0].plot(df["datetime"], df["temperature"], label="Temperature")
     axs[0].set_ylabel("Temperature")
-    axs[0].set_title("Temperature Over Time")
+    axs[0].set_title("Temperature")
     axs[0].legend()
 
     axs[1].plot(df["datetime"], df["humidity"], label="Humidity",
                 color="orange")
     axs[1].set_ylabel("Humidity")
-    axs[1].set_title("Humidity Over Time")
+    axs[1].set_title("Humidity")
     axs[1].legend()
 
     axs[2].plot(df["datetime"], df["pump_status"], label="Pump Status",
                 color="green")
     axs[2].set_ylabel("Pump Status")
-    axs[2].set_title("Pump Status Over Time")
+    axs[2].set_title("Pump Status")
     axs[2].legend()
 
     axs[2].set_xlabel("Datetime")
+    axs[2].xaxis.set_major_formatter(mdates.DateFormatter("%d/%m/%Y %H:%M"))
+    axs[2].xaxis.set_major_locator(mdates.AutoDateLocator())
 
     if save_path:
         plt.savefig(save_path)
